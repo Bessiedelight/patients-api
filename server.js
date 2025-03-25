a8184cbb6f528e41a25b8d57cef6876fcd5ac728
Delight Bessie <147700339+Bessiedelight@users.noreply.github.com>
2025-03-25 01:39:26 +0000

    cors

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); // Import cors
 const patientRoutes = require("./routes/patientRoutes"); // Import the routes
 
 const app = express();
 
-// Middleware to parse JSON requests
+// Define CORS options if you need to customize them.
+const corsOptions = {
+  origin: '*', // Allow requests from any origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// Enable CORS using the options.
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+// Middleware to parse JSON requests.
 app.use(express.json());
 
-// Mount the patient routes under a base path
+// Mount the patient routes under a base path.
 app.use("/api/patients", patientRoutes);
 
-// Connect to MongoDB
+// Connect to MongoDB.
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -23,7 +35,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("Error connecting to MongoDB:", error));
 
-// Start the server
+// Start the server.
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
